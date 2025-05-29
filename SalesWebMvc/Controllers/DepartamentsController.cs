5c95bfad5cae6ce65f782cce6480b7e84be5610f
talitaDias <talita.dias@itriad.org.br>
2025-05-29 14:26:02 -0400

    first mvc - departament

---
+++ SalesWebMvc/Controllers/DepartamentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+using System.Collections.Generic;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartamentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departament> list = new List<Departament>();
+            list.Add(new Departament { Id = 1, Name = "Eletronics"});
+            list.Add(new Departament { Id = 2, Name = "Fashion" });
+
+            return View(list);
+        }
+    }
+}
